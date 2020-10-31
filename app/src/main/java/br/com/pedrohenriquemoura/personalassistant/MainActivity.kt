128c9769b937f2476384b03b2baa9720836ff80f
Pedro Moura <pedromoura@MacBook-Pro-de-Pedro.local>
2020-10-31 16:41:20 -0300

    First Commit

---
+++ app/src/main/java/br/com/pedrohenriquemoura/personalassistant/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.pedrohenriquemoura.personalassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
